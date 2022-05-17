5ceb68afe261c3ead9b54048c1c637099cc86243
Mariana <nanomarii10@gmail.com>
2022-05-17 13:50:30 -0300

    Resolução do exercício 1 do javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/javascript.js
@@ -0,0 +1,10 @@
+//Ecxercício 1 - Fazer cinco programas utilizando a operação aritmética básica.
+const a = 40;
+const b = 10;
+
+console.log('O resultado da soma é: ' + (a+b));
+console.log('O resultado da subtração é: ' + (a-b));
+console.log('O resultado da multiplicação é: ' + (a*b));
+console.log('O resultado da divisão é: ' + (a/b));
+console.log('O resultado do módulo é: ' + (a%b));
+
