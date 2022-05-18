edb1f82d9d7e100d191c30f45547d6a2901d764f
Mariana <nanomarii10@gmail.com>
2022-05-18 00:25:04 -0300

    Resolução do exercício 1 de JS Array e Loop for

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/javascript.js
@@ -0,0 +1,7 @@
+//Array numbers para os exercícios do 1 ao 7.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercício 1 - Percorrer o array imprimindo todos os valores contidos.
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}
