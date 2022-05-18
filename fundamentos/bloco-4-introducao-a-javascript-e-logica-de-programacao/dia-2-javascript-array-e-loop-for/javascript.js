96bbf7441a2d5c901315f4e19390bb663cfd7305
Mariana <nanomarii10@gmail.com>
2022-05-18 11:20:21 -0300

    Resolução do exercício 2 de JS Array e Loop for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/javascript.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/javascript.js
@@ -5,3 +5,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 }
+
+//Exercício 2 - Imprimir o resultado da soma de todos os valores.
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma = soma + numbers[index];
+}
+    console.log('The sum result is:' , soma);
