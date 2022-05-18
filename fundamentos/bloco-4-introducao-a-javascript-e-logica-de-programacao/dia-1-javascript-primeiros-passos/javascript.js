aab608d72047ca34191cdeba31f3dafa87bff7f9
Mariana <nanomarii10@gmail.com>
2022-05-17 22:38:24 -0300

    Resolução do exercício 8 de javascript

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/javascript.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/javascript.js
@@ -1,9 +1,10 @@
-//Ecxercício 1 - Fazer cinco programas utilizando a operação aritmética básica.
+//Valores das constantes usadas nos exercícios.
 const a = 40;
 const b = 10;
 const c = 150;
 const d = -3
 
+//Ecxercício 1 - Fazer cinco programas utilizando a operação aritmética básica.
 console.log('O resultado da soma é: ' + (a+b));
 console.log('O resultado da subtração é: ' + (a-b));
 console.log('O resultado da multiplicação é: ' + (a*b));
@@ -34,4 +35,25 @@ if (d > 0) {
 } else {
     console.log('Zero');
 }
+
+//Exercício 5 - Retornar true se os ângulos representarem os ângulos de um triângulo e false, caso contrário.
+const angleTri1 = 30;
+const angleTri2 = 70;
+const angleTri3 = 80;
+
+const sumOfAngles = angleTri1 + angleTri2 + angleTri3;
+
+
+//Exercício 8 - Retornar true se pelo menos uma das três constantes for par ou false, caso contrário.
+const number1 = 15;
+const number2 = 27;
+const number3 = 70;
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+
 
